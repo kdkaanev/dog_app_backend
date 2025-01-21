b02bc2e5ff0a5ce1ca715505caced6ed5745b964
kdkaanev <kdkaane@github.io>
2025-01-21 15:28:08 +0200

    logout feature is done

--- backend/settings.py
+++ backend/settings.py
@@ -60,7 +60,7 @@ CORS_ALLOWED_ORIGINS = [
 ]
 CORS_ALLOW_CREDENTIALS = True
 CSRF_TRUSTED_ORIGINS = [
-    "http://localhost:5173"
+    "http://localhost:5173",
     "http://127.0.0.1:5173",
 ]
 SESSION_COOKIE_SAMESITE = "Lax"  # Or "None" for cross-site
