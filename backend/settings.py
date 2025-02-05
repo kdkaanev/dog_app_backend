76d0e9ac350e6f0a74e649a9903b6e273b8a5a8d
kdkaanev <kdkaane@github.io>
2025-02-05 20:07:21 +0200

    up database

--- backend/settings.py
+++ backend/settings.py
@@ -13,7 +13,6 @@ import os
 from dotenv import load_dotenv
 from pathlib import Path
 
-
 load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -26,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-#DEBUG = bool(os.getenv("DEBUG", 0))
+# DEBUG = bool(os.getenv("DEBUG", 0))
 DEBUG = False
 
 ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "*").split(",")
@@ -119,8 +118,8 @@ DATABASES = {
 }
 
 '''
-if DEBUG:
-    DATABASES = {
+'''
+ DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": "dog_db1",
@@ -130,8 +129,9 @@ if DEBUG:
         "PORT": "5432",
     }
 }
-else:
-    DATABASES = {
+'''
+
+DATABASES = {
     "default": {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.getenv('POSTGRES_DB'),
