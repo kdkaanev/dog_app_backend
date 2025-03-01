d43d2b94e38ab95ecc7916ddb64693d04fe57015
kdkaanev <kdkaane@github.io>
2025-03-01 13:54:57 +0200

    fix message

--- backend/dog_api/serializers.py
+++ backend/dog_api/serializers.py
@@ -5,13 +5,20 @@ from .models import DogPost, DogUser, Message,Comment
 
 class DogPostSerializer(serializers.ModelSerializer):
     dog_user_id = serializers.SerializerMethodField()
+    dog_user_name = serializers.SerializerMethodField()
+
     class Meta:
         model = DogPost
-        fields = ['id', 'title', 'breed', 'photo_url', 'description', 'last_seen_location', 'date_posted', 'status', 'user', 'dog_user_id']
+        fields = ['id', 'title', 'breed', 'photo_url', 'description', 'last_seen_location', 'date_posted', 'status', 'user', 'dog_user_id', 'dog_user_name', 'has_messages']
         read_only_fields = ('id','date_posted','user', )
     def get_dog_user_id(self, obj):
         dog_user = DogUser.objects.filter(user=obj.user).first()
         return dog_user.id if dog_user else None
+    def get_dog_user_name(self, obj):
+        dog_user = DogUser.objects.filter(user=obj.user).first()
+        return dog_user.first_name if dog_user else None
+
+
 
 
 
